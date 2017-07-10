165760f8c91bb313a1cc76b603144a6dabcc37d0
lzx李子翔 <zixiangli@ctrip.com>
2017-07-10 15:51:13 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+
+import Button from 'antd/lib/button';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Button type="primary">Button</Button>
+            </div>
+        );
+    }
 }
 
 export default App;
