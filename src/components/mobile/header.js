7ceda845f6ef9e55b266abf5a09c005f6a67e3ee
lzx李子翔 <zixiangli@ctrip.com>
2017-07-10 18:02:56 +0800

    完成基础架构 header和footer 按需加载antd

--- src/components/mobile/header.js
+++ src/components/mobile/header.js
@@ -2,3 +2,17 @@
  * Created by zixiangli on 2017/7/10.
  */
 import React from 'react'
+
+class MobileHeader extends React.Component {
+    render() {
+        return (
+            <div id="mobileheader">
+                <header>
+                    <span>LZX News</span>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default MobileHeader;
--- src/components/mobile/index.js
+++ src/components/mobile/index.js
@@ -2,7 +2,7 @@
  * Created by zixiangli on 2017/7/10.
  */
 import React from 'react'
-
+import MobileHeader from './header'
 import MobileFooter from './footer'
 import '../../css/mobile.css'
 
@@ -10,6 +10,7 @@ class MobileIndex extends React.Component {
     render() {
         return (
             <div>
+                <MobileHeader/>
                 <MobileFooter/>
             </div>
         );
