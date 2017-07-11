9320202d17f1455260f2b86b2fa5a4ab729099b7
lzx李子翔 <zixiangli@ctrip.com>
2017-07-11 10:50:30 +0800

    登录注册modal

--- src/components/pc/header.js
+++ src/components/pc/header.js
@@ -2,8 +2,13 @@
  * Created by zixiangli on 2017/7/10.
  */
 import React from 'react'
-import {Row, Col, Menu, Icon} from 'antd'
+import {Row, Col, Menu, Icon, Form, Input, Button, Checkbox, Modal, Tabs, message} from 'antd'
+import LoginForm from './loginForm'
+import RegisterForm from './registerForm'
+
 const {SubMenu, MenuItemGroup} = Menu;
+//注册登录表单相关，另外用到了Modal模态窗、选项卡Tabs、message全局提示
+const {TabPane} = Tabs;//选项卡
 
 class PcHeader extends React.Component {
     constructor(props) {
@@ -13,15 +18,35 @@ class PcHeader extends React.Component {
         }
     }
 
-    handleClick(e) {
-        this.setState({
-            current: e.key,
-        });
+    handleClick(e) { //响应头部tab ，e是个对象
+        if (e.key === 'userTab') {
+
+        } else {
+            this.setState({
+                current: e.key,
+            });
+        }
     };
 
     render() {
         return (
             <div>
+                {/* Modal */}
+                <Modal title="用户中心"
+                       visible={true}
+                       onCancel={true}
+                       footer={null}
+                >
+                    <Tabs defaultActiveKey="login">
+                        <TabPane tab={<span><Icon type="login"/>登录</span>} key="login">
+                            <LoginForm/>
+                        </TabPane>
+                        <TabPane tab={<span><Icon type="user-add"/>注册</span>} key="register">
+                            <RegisterForm/>
+                        </TabPane>
+                    </Tabs>
+                </Modal>
+                {/*头部tab*/}
                 <Row>
                     <Col span={2}></Col>
                     <Col span={4}>
@@ -57,6 +82,9 @@ class PcHeader extends React.Component {
                             </Menu.Item>
                             <Menu.Item key="shishang">
                                 <Icon type="apple"/>时尚
+                            </Menu.Item>
+                            <Menu.Item className="userTab" key="userTab">
+                                <Icon type="login"/>注册/登录
                             </Menu.Item>
                         </Menu>
                     </Col>
---
+++ src/components/pc/registerForm.js
@@ -0,0 +1,178 @@
+/**
+ * Created by zixiangli on 2017/7/11.
+ */
+import React from 'react'
+import {Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete} from 'antd';
+const FormItem = Form.Item;
+const Option = Select.Option;
+const AutoCompleteOption = AutoComplete.Option;
+
+const residences = [{
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [{
+        value: 'hangzhou',
+        label: 'Hangzhou',
+        children: [{
+            value: 'xihu',
+            label: 'West Lake',
+        }],
+    }],
+}, {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [{
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [{
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+        }],
+    }],
+}];
+
+class RegistrationForm extends React.Component {
+    state = {
+        confirmDirty: false,
+        autoCompleteResult: [],
+    };
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
+    handleConfirmBlur = (e) => {
+        const value = e.target.value;
+        this.setState({confirmDirty: this.state.confirmDirty || !!value});
+    };
+    checkPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('Two passwords that you enter is inconsistent!');
+        } else {
+            callback();
+        }
+    };
+    checkConfirm = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['confirm'], {force: true});
+        }
+        callback();
+    };
+
+    handleWebsiteChange = (value) => {
+        let autoCompleteResult;
+        if (!value) {
+            autoCompleteResult = [];
+        } else {
+            autoCompleteResult = ['.com', '.org', '.net'].map(domain => `${value}${domain}`);
+        }
+        this.setState({autoCompleteResult});
+    };
+
+    render() {
+        const {getFieldDecorator} = this.props.form;
+        const {autoCompleteResult} = this.state;
+
+        const formItemLayout = {
+            labelCol: {
+                xs: {span: 24},
+                sm: {span: 6},
+            },
+            wrapperCol: {
+                xs: {span: 24},
+                sm: {span: 14},
+            },
+        };
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0,
+                },
+                sm: {
+                    span: 14,
+                    offset: 6,
+                },
+            },
+        };
+        const prefixSelector = getFieldDecorator('prefix', {
+            initialValue: '86',
+        })(
+            <Select style={{width: 60}}>
+                <Option value="86">+86</Option>
+                <Option value="87">+87</Option>
+            </Select>
+        );
+
+        const websiteOptions = autoCompleteResult.map(website => (
+            <AutoCompleteOption key={website}>{website}</AutoCompleteOption>
+        ));
+
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <FormItem
+                    {...formItemLayout}
+                    label="用户名"
+                    hasFeedback
+                >
+                    {getFieldDecorator('userName', {
+                        rules: [{
+                            required: true, message: '请输入用户名!',
+                        }],
+                    })(
+                        <Input />
+                    )}
+                </FormItem>
+                <FormItem
+                    {...formItemLayout}
+                    label="密码"
+                    hasFeedback
+                >
+                    {getFieldDecorator('password', {
+                        rules: [{
+                            required: true, message: '请输入密码!',
+                        }, {
+                            validator: this.checkConfirm,
+                        }],
+                    })(
+                        <Input type="password"/>
+                    )}
+                </FormItem>
+                <FormItem
+                    {...formItemLayout}
+                    label="确认密码"
+                    hasFeedback
+                >
+                    {getFieldDecorator('confirm', {
+                        rules: [{
+                            required: true, message: '请再次输入密码!',
+                        }, {
+                            validator: this.checkPassword,
+                        }],
+                    })(
+                        <Input type="password" onBlur={this.handleConfirmBlur}/>
+                    )}
+                </FormItem>
+                <FormItem {...tailFormItemLayout} style={{marginBottom: 8}}>
+                    {getFieldDecorator('agreement', {
+                        valuePropName: 'checked',
+                    })(
+                        <Checkbox>我已阅读并同意<a href="">用户协议</a></Checkbox>
+                    )}
+                </FormItem>
+                <FormItem {...tailFormItemLayout}>
+                    <Button style={{'width': '100%'}} type="primary" htmlType="submit" size="large">注册</Button>
+                </FormItem>
+            </Form>
+        );
+    }
+}
+
+const RegisterForm = Form.create()(RegistrationForm);
+
+export default RegisterForm;
